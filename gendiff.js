02f2418805feafa84c1c30c55a3d5eb4fa5de0a1
Amina Iskhakova <amina272726@yandex.ru>
2025-06-11 22:51:23 +0300

    Task 2: added arguments, --format

--- gendiff.js
+++ gendiff.js
@@ -1,7 +1,12 @@
-const { program } = require('commander');
+import { Command } from 'commander';
+
+const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
+  .argument('<filepath1>', 'first filepath')
+  .argument('<filepath2>', 'second filepath')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
