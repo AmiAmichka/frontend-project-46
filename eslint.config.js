896f0bfe218636fa26041d9201df857f5d2595cd
Amina Iskhakova <amina272726@yandex.ru>
2025-12-04 14:12:01 +0300

    fix linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
-import globals from 'globals';
+import globals from 'globals'
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginJs from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+import importPlugin from 'eslint-plugin-import'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: pluginJs.configs.recommended,
-});
+})
 
 export default [
   {
@@ -54,6 +54,9 @@ export default [
       'no-console': 'off',
       'import/no-extraneous-dependencies': 'off',
       'object-curly-newline': 'off',
+      'arrow-parens': [2, 'as-needed', { requireForBlockBody: true }],
+      semi: ['error', 'never'],
+      'brace-style': ['error', 'stroustrup'],
     },
   },
-];
+]
